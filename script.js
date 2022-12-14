5b866551d58e3d8928c85c1854d99609b18335e0
jakubkrnac <jakub@krn.ac>
2022-12-14 14:10:48 +0100

    Add comments

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 */
 
 let state = {
-    timeOfLastEvent: 0
+    timeOfLastEvent: 0 // defines when was the cursor lastly moved
 }
 
 const settings = Object.freeze({
@@ -27,7 +27,7 @@ const settings = Object.freeze({
     },
     liquid: {
         color: '#ffffff',
-        viscosity: 1 // the bigger the viscosity, the slower will the waves travel
+        viscosity: 1 // the bigger the viscosity, the slower will the waves travel. viscosity set to 1 represents generic liquid
     }
 });
 
@@ -41,35 +41,35 @@ function updateState(timeOfEvent) {
     });
 }
 
-let eventCounter = 0;
+let eventCounter = 0; //variable to count events
 
 /**
  * @param {PointerEvent} event An object with the properties to update in the state object.
  */
 function handlePointerEvent(event) {
     if (eventCounter <= settings.skipEvents) {
-        eventCounter++;
-    } else {
-        const {viscosity} = settings.liquid;
-        const {color} = settings.wave;
-        let wave = document.createElement('div');
-        wave.style.left = event.x + 'px';
-        wave.style.top = event.y + 'px';
+        eventCounter++; // => eventCounter + 1
+    } else { // if 30 events have been skipped, program proceeds to generate a wave
+        const {viscosity} = settings.liquid; // get liquid viscosity from settings object
+        const {color} = settings.wave; // get wave color from settings object
+        let wave = document.createElement('div'); // create div element, div is not added to the page yet!
+        wave.style.left = event.x + 'px'; // set div position from left
+        wave.style.top = event.y + 'px'; // set div position from top
         wave.style.border = '1px solid ' + color;
-        wave.style.transition = getWaveTransition(viscosity);
-        document.body.append(wave);
+        wave.style.transition = getWaveTransition(viscosity); // set css transition
+        document.body.append(wave); // add this div to our body
 
         setTimeout(function () {
-            wave.classList.add('expand');
-        }, 0);
+            wave.classList.add('expand'); // add class expand to the div element to start the transformation
+        }, 0); // it has to be done like this since it is being called from javascript
         //wave.classList.add('expand');
 
-        eventCounter = 0;
+        eventCounter = 0; // reset event counter
     }
 }
 
-function getWaveTransition(viscosity) {
-    return (viscosity * 2) + 's ease-out';
+function getWaveTransition(viscosity) { // viscosity * 2 is our default translation from viscosity to time
+    return (viscosity * 2) + 's ease-out'; // generates transition css code (e.g. 2s ease-out)
 }
 function getWaveTravelTimeMillis(viscosity) {
     return viscosity * 2 * 1000;
@@ -80,10 +80,10 @@ function getWaveTravelTimeMillis(viscosity) {
  * loop() is run every frame, assuming that we keep calling it with `window.requestAnimationFrame`.
  */
 function loop() {
-    const { timeOfLastEvent } = state;
-    const { viscosity } = settings.liquid;
+    const { timeOfLastEvent } = state; // get timeOfLastEvent from the state object
+    const { viscosity } = settings.liquid; // get viscosity from settings
 
-    if ((new Date().getTime() - getWaveTravelTimeMillis(viscosity)) > timeOfLastEvent) {
+    if ((new Date().getTime() - getWaveTravelTimeMillis(viscosity)) > timeOfLastEvent) { // this is executed when all waves dissappear
         document.body.innerHTML = ''; //clear our body
     }
 
@@ -94,11 +94,11 @@ function loop() {
  * Setup is run once, at the start of the program. It sets everything up for us!
  */
 function setup() {
-    const {color} = settings.liquid;
+    const {color} = settings.liquid; // get liquid color from settings
 
-    document.body.style.backgroundColor = color;
+    document.body.style.backgroundColor = color; // set the color from settings to the body
     document.addEventListener('pointermove', function (event) {
-        updateState(new Date().getTime());
+        updateState(new Date().getTime()); // call updateState with current time as a parameter
         handlePointerEvent(event);
     });
 
